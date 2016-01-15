9e29a91b9874de994028aab62781a1ab74567cf9
Jonathan Lozano Hdz <jon_2194@hotmail.com>
2016-01-15 12:55:06 -0600

    00-Saludo
    Actividad 1 del dia 13/01/2016

---
+++ src/Saludo.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Jonathan Lozano
+ *
+ */
+public class Saludo {
+
+	/**
+	 * 
+	 * @param args
+	 * Se impirme un mensaje predefinido
+	 */
+	public static void main(String[] args) 
+	{
+		JOptionPane.showMessageDialog(null, "Jonathan");
+		System.out.println("Jonathan");
+	}
+
+}
